eb0468a4e8c616c0f91fdd6f7f2410a5d9bc84c6
DaniM0613 <wwh.rj.92@gmail.com>
2024-06-28 16:03:29 -0600

    coding the login part and trying it with postman and MongoDB

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import { generateTokenAndSetCookie } from '../lib/utils/generateToken.js';
-import User from '../models/user.model.js'
-import bcrypt from 'bcryptjs';
+import User from '../models/user.model.js';
+import bcrypt from 'bcrypt'
 
 export const signup = async (req, res) => {
     try {
@@ -11,16 +11,19 @@ export const signup = async (req, res) => {
             return res.status(400).json({error: 'Invalid email format'});
         }
         
-        const existingUser = await userName.findOne({userName});
+        const existingUser = await User.findOne({userName});
             if(existingUser){
                 return res.status(400).json({error: 'Username is already taken' });
         }
         
-        const existingEmail = await userName.findOne({email});
+        const existingEmail = await User.findOne({email});
         if(existingEmail){
             return res.status(400).json({error: 'Email is already taken'});     
         }
 
+        if(password.length < 6){
+            return res.status(400).json({error: 'Password must be at least 6 character long'})
+        }
         //HASH PASSWORD
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
@@ -56,9 +59,33 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    res.json({
-        data: 'You hit the login endpoint'
-    })
+    try {
+        const { userName, password} = req.body;
+        const user = await User.findOne({userName});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error: 'Invalid username or password '})
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            userName: user.userName,
+            email: user.email,
+            followers: user.followers,
+            following: user.following,
+            profileImg: user.profileImg,
+            coverImg: user.coverImg,
+        })
+        
+
+    } catch (error){
+        console.log('Error in login controller', error.message);
+        res.status(500).json({error: 'Internal Server Error'})
+    }
 }
 
 export const logout = async (req, res) => {
