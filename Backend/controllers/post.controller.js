012e90ee514b79f7a9f7a2b9ec9f7a9d67b84cd0
DaniM0613 <wwh.rj.92@gmail.com>
2024-07-04 15:51:08 -0600

    creating the code for like and unlike a post in twitter

--- Backend/controllers/post.controller.js
+++ Backend/controllers/post.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import Post from "../models/post.model.js";
 import {v2 as cloudinary} from 'cloudinary';
+import Notification from "../models/notification.model.js";
 
 export const createPost = async (req, res) => {
     try {
@@ -80,5 +81,41 @@ export const commentOnPost = async (req, res) => {
     }catch {
        console.log('Error in commentOnPost controller:', error);
        res.status(500).json({error: 'Internal server error'})
+    }
+};
+
+export const linkUnlikePost = async (req, res) => {
+    try{
+      const userId = req.user._id;
+      const {id:postId} = req.params;
+
+      const post = await Post.findById(postId);
+
+      if(!post){
+        return res.status(404).json({error: 'Post not found'})
+      }
+
+      const userLikedPost = post.likes.includes(userId);
+      if(userLikedPost){
+        //Unlike post
+        await Post.updateOne({_id:postId}, {$pull: {likes: userId}})
+        res.status(200).json({message: 'Post unlike sucessfully'})
+      }else {
+        // Like Post
+        post.likes.push(userId);
+        await post.save();
+
+        const notification = new Notification({
+            from: userId,
+            to: post.user,
+            type:'like'
+        })
+        await notification.save();
+
+        res.status(200).json({message: 'Post liked successfully'})
+      }
+    }catch (error){ 
+        console.log('Error in likeUnlikePost controller:', error)
+        res.status(500).json({error: 'Internal Server error'})
     }
 }
