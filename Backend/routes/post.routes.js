cc2640a96ca2272086388653467095b50a2b77e5
DaniM0613 <wwh.rj.92@gmail.com>
2024-07-04 18:20:46 -0600

    creating the code of getting the likes and the post of an user

--- Backend/routes/post.routes.js
+++ Backend/routes/post.routes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { protectRoute } from '../middleware/protectRoute.js';
-import { createPost, deletePost, commentOnPost, linkUnlikePost, getAllPosts, getLikesPosts} from '../controllers/post.controller.js';
+import { createPost, deletePost, commentOnPost, linkUnlikePost, getAllPosts, getLikesPosts, getFollowingPosts, getUserPosts} from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.get('/all', protectRoute, getAllPosts);
+router.get('/following', protectRoute, getFollowingPosts)
 router.get('/likes/:id', protectRoute, getLikesPosts);
+router.get('/user/:username', protectRoute, getUserPosts)
 router.post('/create', protectRoute, createPost);
 router.post('/like/:id', protectRoute, linkUnlikePost);
 router.post('/comment/:id', protectRoute, commentOnPost);
