1dad1d2163d2bcfb1a166c3970a370cd96455484
DaniM0613 <wwh.rj.92@gmail.com>
2024-07-25 21:43:34 -0600

    prepared our app for deployement

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+const __dirname = path.resolve()
 
 app.use(express.json({limit:"5mb"})); //  to parse req.bodykb
 // limit shouldnt be too high to prevent DOS
